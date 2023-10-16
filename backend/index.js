db6b0a531ed8200a2a928cb7c2fe0dd7e4c78bea
Eric Thomas <ericthomas@fetch.ly>
2023-10-16 17:20:02 -0300

    project is set up to build

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,17 @@ const app = express();
 
 app.get("/", (req, res, next) => {
     res.sendFile(path.resolve(__dirname + "/../frontend/public/index.html"));
-})
+});
+
+app.get("/login", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname + "/../frontend/public/index.html"));
+});
+
+app.get("/app/*", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname + "/../frontend/public/index.html"));
+});
+
+app.use('/static', express.static(path.join(__dirname, '/../frontend/public')));
 
 app.listen(process.env.BACKEND_PORT, () => {
     console.log(`Server running on port ${process.env.BACKEND_PORT},${__dirname}`)
