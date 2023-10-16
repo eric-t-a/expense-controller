306a625215c19de46d400c5b72151ff4e08cc1c5
Eric Thomas <ericthomas@fetch.ly>
2023-10-16 16:10:11 -0300

    init commit

--- expenses_controller/api/routes.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import FlowView
-
-urlpatterns = [
-    path('', FlowView.as_view()),
-]
--- expenses_controller/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import generics
-from .serializers import FlowSerializer
-from .models import Flow
-
-class FlowView(generics.CreateAPIView):
-    queryset = Flow.objects.all()
-    serializer_class = FlowSerializer
